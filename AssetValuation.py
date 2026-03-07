89952e0413c0cd2742977548bdaa2bcc17fb1ea2
kevinmillercheme <kevinmiller1242@gmail.com>
2026-03-07 18:32:19 -0500

    Added Yahoo Finance (yfinance) API scraping for real-time asset data depending on user input. Returning key values for later model calculations

--- AssetValuation.py
+++ AssetValuation.py
@@ -1,25 +1,68 @@
-asset = 'NVDA'
-price = 186 # current price
-dividend = 0.0002 # current div ratio
-marketCap = 4.58e9 # current market cap
-PEratio = 45.28 # current PE ratio
-PBratio = 38 # current PB
-EGR = 0.6426 # 5-Year EGR for stability
+# '''
+# Endpoint: https://paper-api.alpaca.markets/v2
+# Key: PKM6SICFKQ7P6FKSXYXVP2OAG4
+# Secret: 7XFKjFipcTKhLmthTb7vgoTV75QwWu1dCsqBo3SVj152
+# '''
 
-# PEG Ratio = PEratio / Earnings Growth Rate (EGR)
+# from alpaca.data.historical import CryptoHistoricalDataClient
+# from alpaca.data.requests import CryptoBarsRequest
+# from alpaca.data.timeframe import TimeFrame
+# import datetime
 
-# Linear regression is used for socioeconomic impact
-def marketCapEval(marketCap):
-    if marketCap > 1e6:
-        mcfactor = 1
-        print('solid', mcfactor)
-    elif marketCap < 1e6 and marketCap > 1e3:
-        mcfactor = 0.8
-        print('decent', mcfactor)
-    elif marketCap > 1e2:
-        mcfactor = 0.6
-        print('no', mcfactor)
-    else:
-        mcfactor = 0.25
-        print('bad', mcfactor)
+# # No keys required for crypto data
+# client = CryptoHistoricalDataClient()
 
+# # Creating request object
+# request_params = CryptoBarsRequest(
+#   symbol_or_symbols=["BTC/USD"],
+#   timeframe=TimeFrame.Day,
+#   start=datetime.datetime(2026, 3, 1),
+#   end=datetime.datetime(2026, 3, 7)
+# )
+
+# # Retrieve daily bars for Bitcoin in a DataFrame and printing it
+# btc_bars = client.get_crypto_bars(request_params)
+
+# # Convert to dataframe
+# btc_bars.df
+
+# timestamps = btc_bars["BTC/USD"]
+
+# ASSETS #
+
+# from alpaca.trading.client import TradingClient
+# from alpaca.trading.requests import GetAssetsRequest
+# from alpaca.trading.enums import AssetClass
+
+# trading_client = TradingClient('PKM6SICFKQ7P6FKSXYXVP2OAG4', '7XFKjFipcTKhLmthTb7vgoTV75QwWu1dCsqBo3SVj152')
+
+# # search for US equities
+# search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
+
+# assets = trading_client.get_all_assets(search_params)
+# # print(assets)
+
+# # search for AAPL
+# aapl_asset = trading_client.get_asset('AAPL')
+
+# if aapl_asset.tradable:
+#     print(aapl_asset)
+
+import yfinance as yf
+
+userinput = input('Enter the asset to analyze: ')
+
+ticker = yf.Ticker(f"{userinput}")
+info = ticker.info
+
+# print(info)
+
+marketcap = f"{info["marketCap"]:,}"
+print(f"Asset Entered: {info["displayName"]}")
+print(f"Market Cap: ${marketcap}")
+print(f"Dividend Yield: {info["dividendYield"]}%")
+print(f"P/E Ratio: {info["trailingPE"]}")
+print(f"52 Week High: ${info["fiftyTwoWeekHigh"]}")
+print(f"52 Week Low: ${info["fiftyTwoWeekLow"]}")
+print(f"Open: ${info["open"]}")
+print(f"Previous Close: ${info["previousClose"]}")
